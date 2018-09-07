71732044a23d4fb85f2b7446a8bf6829a5f87231
WDB40 <wbrown1640@gmail.com>
2018-09-06 21:57:54 -0500

    Make Person class an Entity and add PersonRepository interface

--- src/main/java/validatingForms/Model/Person.java
+++ src/main/java/validatingForms/Model/Person.java
@@ -1,10 +1,19 @@
 package validatingForms.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
     @NotNull
     @Size(min=2, max=30)
@@ -28,6 +37,14 @@ public class Person {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     @Override
---
+++ src/main/java/validatingForms/Model/PersonRepository.java
@@ -0,0 +1,6 @@
+package validatingForms.Model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Integer> {
+}
